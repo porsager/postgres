618323f74ec9c2005741eb6860074b1856b1b7b1
Rasmus Porsager <rasmus@porsager.com>
2019-12-11 12:02:04 +0100

    higher timeout (ssl test was slow)

--- tests/test.js
+++ tests/test.js
@@ -20,7 +20,7 @@ async function test(o, name, fn, after) {
 
   tests[line] = { fn, line, name }
   promise = promise.then(() => Promise.race([
-    new Promise((resolve, reject) => fn.timer = setTimeout(() => reject('Timed out'), 100)),
+    new Promise((resolve, reject) => fn.timer = setTimeout(() => reject('Timed out'), 500)),
     fn()
   ]))
     .then(([expected, got]) => {
