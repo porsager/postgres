c6bf6be83d10a90c2921b2e476bbf06edc76e99a
Rasmus Porsager <rasmus@porsager.com>
2023-02-01 20:42:35 +0100

    Use final string for simple statements too - fixes #532

--- src/connection.js
+++ src/connection.js
@@ -180,7 +180,7 @@ function Connection(options, queues = {}, { onopen = noop, onend = noop, onclose
       throw Errors.generic('MAX_PARAMETERS_EXCEEDED', 'Max number of parameters (65534) exceeded')
 
     return q.options.simple
-      ? b().Q().str(q.strings[0] + b.N).end()
+      ? b().Q().str(q.statement.string + b.N).end()
       : q.describeFirst
         ? Buffer.concat([describe(q), Flush])
         : q.prepare
