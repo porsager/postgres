48877662154180ec17736b5db4c44e17a63e486d
Rasmus Porsager <rasmus@porsager.com>
2023-05-17 02:56:22 +0200

    Not ready for tests on CF yet

--- transpile.cf.js
+++ transpile.cf.js
@@ -5,24 +5,13 @@ const empty = x => fs.readdirSync(x).forEach(f => fs.unlinkSync(path.join(x, f))
     , ensureEmpty = x => !fs.existsSync(x) ? fs.mkdirSync(x) : empty(x)
     , root = 'cf'
     , src = path.join(root, 'src')
-    , tests = path.join(root, 'tests')
 
 ensureEmpty(src)
-ensureEmpty(tests)
 
 fs.readdirSync('src').forEach(name =>
   fs.writeFileSync(
     path.join(src, name),
     transpile(fs.readFileSync(path.join('src', name), 'utf8'), name, 'src')
-  )
-)
-
-fs.readdirSync('tests').forEach(name =>
-  fs.writeFileSync(
-    path.join(tests, name),
-    name.endsWith('.js')
-      ? transpile(fs.readFileSync(path.join('tests', name), 'utf8'), name, 'tests')
-      : fs.readFileSync(path.join('tests', name), 'utf8')
   )
 )
 
