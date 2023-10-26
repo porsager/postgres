f2fb819de4078ec6ff3e4dbf6c5dc117c2d5b0a0
Pyrolistical <pyrogx1133@gmail.com>
2023-10-26 15:53:42 -0700

    Keep query error instead of creating creating new object (#698)
    * Keep query error instead of creating creating new object
    
    fixes #696
    
    * Enumerate properties only if debug
    
    * Fixed typo
    
    * Fixed styling

--- src/connection.js
+++ src/connection.js
@@ -385,13 +385,14 @@ function Connection(options, queues = {}, { onopen = noop, onend = noop, onclose
   }
 
   function queryError(query, err) {
-    query.reject(Object.create(err, {
+    Object.defineProperties(err, {
       stack: { value: err.stack + query.origin.replace(/.*\n/, '\n'), enumerable: options.debug },
       query: { value: query.string, enumerable: options.debug },
       parameters: { value: query.parameters, enumerable: options.debug },
       args: { value: query.args, enumerable: options.debug },
       types: { value: query.statement && query.statement.types, enumerable: options.debug }
-    }))
+    })
+    query.reject(err)
   }
 
   function end() {
