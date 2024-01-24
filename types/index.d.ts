5404fbd6bcd145e604bc309b2e1a7cb49ceaed25
louis.tian <louis.tian@aquamonix.com.au>
2024-01-25 09:25:10 +1100

    update type definition

--- types/index.d.ts
+++ types/index.d.ts
@@ -690,7 +690,7 @@ declare namespace postgres {
     listen(channel: string, onnotify: (value: string) => void, onlisten?: (() => void) | undefined): ListenRequest;
     notify(channel: string, payload: string): PendingRequest;
 
-    subscribe(event: string, cb: (row: Row | null, info: ReplicationEvent) => void, onsubscribe?: (() => void) | undefined): Promise<SubscriptionHandle>;
+    subscribe(event: string, cb: (row: Row | null, info: ReplicationEvent) => void, onsubscribe?: (() => void), onerror?: (() => any)): Promise<SubscriptionHandle>;
 
     largeObject(oid?: number | undefined, /** @default 0x00020000 | 0x00040000 */ mode?: number | undefined): Promise<LargeObject>;
 
